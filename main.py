38973db5d6fd77b71b15167255e077808164e3c1
peter.ak <peter>
2024-06-12 21:51:00 +0200

    q2 has been completed

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
-from datetime import datetime
-
 import pandas as pd
 from pandas import DataFrame
 
-# import data set form excel file
+from q2 import avg_salary
+
+# import data set form excel file and fix white spaces
 data_set: DataFrame = pd.read_excel("./raw_data/data-test.xlsx")
+data_set.columns = [c.replace(' ', '_') for c in data_set.columns]
 
 ##################################
 # Q1 -- Average age by department
 ##################################
 
-
 ##################################
-# Q2 -- salaire moyen en fonction du nombre d’années d’expérience
+# Q2 -- Average salary by years of experience
 ##################################
-data_set["today_date"] = datetime.today()
-data_set["delta_days_contract_start"] = data_set["today_date"] - data_set["DEBUT_CONTRAT"]
-print(data_set)
+avg_salary(data_set=data_set)
---
+++ q2.py
@@ -0,0 +1,47 @@
+from datetime import datetime, timedelta
+
+import pandas as pd
+from pandas import DataFrame, Series
+import matplotlib.pyplot as plt
+
+
+def avg_salary(data_set: DataFrame) -> None:
+    """Fonction qui permet de determiner le salaire moyen
+    en fonction du nombre d'années d'expérience
+
+    Args:
+        data_set (DataFrame): L'ensemble de données fourni pour la question
+    """
+
+    # Remove any faulty dates from the column
+    data_set["DEBUT_CONTRAT"] = pd.to_datetime(
+        data_set["DEBUT_CONTRAT"],
+        errors="coerce",
+    )
+    data_set.dropna(inplace=True, subset=["DEBUT_CONTRAT"])
+
+    # Gets the delta years between start date and tody
+    data_set["delta_contract_days"] = datetime.today() - data_set["DEBUT_CONTRAT"]
+    data_set["delta_contract_years"] = data_set["delta_contract_days"] / timedelta(
+        days=362
+    )
+
+    # Bins and groups data by the delta years column
+    result_set: Series = data_set.groupby(
+        pd.cut(
+            data_set["delta_contract_years"],
+            5,
+        ),
+        observed=True,
+    )["TAUX_HORAIRE_BRUT"].mean()
+
+    print(result_set)
+
+    # Plot results
+    result_set.plot.bar(
+        rot=False,
+        title="salaire moyen en fonction d'années d'expérience",
+    )
+    plt.ylabel("salaire moyen [Eur/heure]")
+    plt.xlabel("années d'expérience [années]")
+    plt.show()
